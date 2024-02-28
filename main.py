8fde40d408446840d9fa08867c21ba391d7a1941
lordofmet <86737218+lordofmet@users.noreply.github.com>
2024-02-28 23:02:23 +0300

    initial connected to db

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import telebot
 import sqlite3
+import datetime
+import time
+from threading import Thread
+import pickle
 
 connect = sqlite3.connect("/Users/dmirt/PycharmProjects/db", check_same_thread=False)
 curs = connect.cursor()
 print(connect)
 
-def db_table_add(user_id: int, username: str, day: int, month: int, year: int):
-    curs.execute('INSERT INTO test (user_id, username, day, month, year) VALUES(?, ?, ?, ?, ?)', (user_id, username,
-                                                                                                   day, month, year))
+curs.execute('''  
+CREATE TABLE IF NOT EXISTS users (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT
+                     UNIQUE
+                     NOT NULL,
+    user_id  INTEGER UNIQUE
+                     NOT NULL,
+    username TEXT    NOT NULL,
+    day      INTEGER NOT NULL,
+    month    INTEGER NOT NULL,
+    year     INTEGER NOT NULL,
+    name     TEXT    NOT NULL,
+    tg_msg   TEXT    NOT NULL
+);
+''')
+
+def db_table_add(user_id: int, username: str, day: int, month: int, year: int, name: str, tg_msg: str):
+    curs.execute('INSERT INTO users (user_id, username, day, month, year, name, tg_msg) VALUES(?, ?, ?, ?, ?, ?, ?)',
+                                    (user_id, username,day, month, year, name, tg_msg))
     connect.commit()
 
 def readFileToStr(file):
@@ -37,9 +57,21 @@ def add(message):
     bot.register_next_step_handler(msg, got_add_query)
 
 def got_add_query(msg):
-    s = msg.text
+    s1 = msg.text.split(' ')
+
+    if (len(s1) != 2):
+        print(len(s1))
+        print(s1)
+        warning = "Некорректный ввод имени"
+        bot.send_message(msg.chat.id, warning)
+        add(msg)
+        return
+
+    s = s1[0]
 
     if (len(s) != 8):
+        print(len(s))
+        print(s)
         warning = "Некорректный ввод даты"
         bot.send_message(msg.chat.id, warning)
         add(msg)
@@ -84,8 +116,117 @@ def got_add_query(msg):
     day = int(day)
     month = int(month)
     year = int(year)
+    name = s1[1]
+    tg_msg = pickle.dumps(msg)
+    print("deserealized: ", tg_msg)
 
-    db_table_add(uid, uname, day, month, year)
+    db_table_add(uid, uname, day, month, year, name, tg_msg)
     bot.send_message(msg.chat.id, 'Ваша дата была добавлена в БД')
 
-bot.polling(none_stop = True, interval = 0)
+    #check_date(msg)
+
+def polling():
+    bot.polling(none_stop = True, interval = 0)
+
+'''def check_date(msg):
+    check_date_thread = Thread(target=check_date(msg))
+
+    curT = str(datetime.datetime.now())
+
+    curT = curT.split(' ')
+    print(curT)
+
+    cur_time = curT[1]
+    #cur_time = "23:59:59"
+    cur_time = cur_time.split(':')
+
+    hours_to_sleep = 0
+    minutes_to_sleep = 0
+    seconds_to_sleep = 0
+
+    hours_to_sleep = 24 - int(cur_time[0])
+
+    if (int(cur_time[1]) != 0):
+        hours_to_sleep -= 1
+        if (hours_to_sleep == -1):
+            hours_to_sleep = 23
+        minutes_to_sleep += 60 - int(cur_time[1])
+
+    if (minutes_to_sleep == 60):
+        minutes_to_sleep = 0
+        hours_to_sleep += 1
+
+    if (float(cur_time[2]) >= 1e-15):
+        minutes_to_sleep -= 1
+        if (minutes_to_sleep == -1):
+            hours_to_sleep = 23
+            minutes_to_sleep = 59
+        seconds_to_sleep += 60 - float(cur_time[2])
+
+    print(hours_to_sleep, minutes_to_sleep, seconds_to_sleep)
+
+    check_date_thread.start()
+
+    await_time = seconds_to_sleep + minutes_to_sleep * 60 + hours_to_sleep * 3600
+    print("waiting for", await_time)
+    time.sleep(await_time)
+
+    curT = str(datetime.datetime.now())
+    curT = curT.split(' ')
+    print(curT)
+
+    cur_date = curT[0]
+    cur_date = cur_date.split('-')
+    cur_day = int(cur_date[2])
+    cur_month = int(cur_date[1])
+    cur_year = int(cur_date[0])
+    print("new date:", cur_day, cur_month)
+
+    for person in curs.execute("SELECT day, month, year, name FROM users"):
+        day = person[0]
+        month = person[1]
+        year = person[2]
+        name = person[3]
+        if (cur_day == day and cur_month == month):
+            f = "Поздравляем" + str(name) + "ему(ей) " + str(int(cur_year - year))
+            bot.send_message(msg.chat.id, f)
+'''
+
+def check_dates():
+    while True:
+        for person in curs.execute("SELECT day, month, year, name, tg_msg FROM users"):
+            tday = person[0]
+            tmonth = person[1]
+            tyear = person[2]
+            tname = person[3]
+
+            tmsg = person[4]
+            tg_msg = pickle.loads(tmsg)
+
+            curT = str(datetime.datetime.now())
+            curT = curT.split(' ')
+            cur_date = curT[0]
+            cur_date = cur_date.split('-')
+            cur_day = int(cur_date[2])
+            cur_month = int(cur_date[1])
+            cur_year = int(cur_date[0])
+
+            if cur_day == tday and cur_month == tmonth:
+                print("there is some match")
+                if tyear > 24:
+                    tyear = 1900 + tyear
+                else:
+                    tyear = 2000 + tyear
+                f = "Поздравляем " + str(tname) + " ему(ей) " + str(int(cur_year - tyear))
+                bot.send_message(tg_msg.chat.id, f)
+            else:
+                print("not a match")
+        time.sleep(60)
+
+
+
+polling_thread = Thread(target = polling)
+check_dates_thread = Thread(target = check_dates)
+
+check_dates_thread.start()
+polling_thread.start()
--- test_features.py
+++ test_features.py
@@ -1,15 +1,3 @@
-arr = [5, 15, 12]
-print(str(arr))
-k = str(arr)
-print(k)
+string = "00"
 
-a = [ [0] * 10 for i in range(10) ]
-for i in range(10):
-    print(a[i])
-
-
-print()
-a[0].append(5)
-
-for i in range(10):
-    print(a[i])
+print(int(string))
